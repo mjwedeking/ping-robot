e7b084c2e09312326ae89ca360054dc852c35f19
Matthew Wedeking <mjwedeking@gmail.com>
2013-11-22 09:13:41 -0800

    Update ping_robot.cpp
    Talking with wave shield

--- ping_robot.cpp
+++ ping_robot.cpp
@@ -1,5 +1,9 @@
+#include <SoftwareServo.h>
+#include <WaveHC.h>
+#include <WaveUtil.h>
+
 #include <Ping.h>  //include the ping)) libs
-#include <Servo.h> // includes the servo libs
+//#include <Servo.h> // includes the servo libs
 
 #define LEFTSERVOPIN  9
 #define RIGHTSERVOPIN  6
@@ -8,25 +12,53 @@
 #define LEFTSERVOCALIB 52  //servo zero out
 #define RIGHTSERVOCALIB 79  //servo zero out
 
-Servo leftServo;       // name the servo
-Servo rightServo;      // name the servo
-Servo headServo;
+SoftwareServo leftServo;       // name the servo
+SoftwareServo rightServo;      // name the servo
+SoftwareServo headServo;
 Ping ping = Ping(PINGPIN);
+
+SdReader card;
+FatVolume vol;
+FatReader root;
+FatReader file;
+WaveHC wave;
+#define error(msg) error_P(PSTR(msg))
 
 int speed = 10;
 
 void setup(){
-   pinMode(RIGHTSERVOPIN,OUTPUT);
-   pinMode(LEFTSERVOPIN,OUTPUT);
-   pinMode(HEADSERVOPIN,OUTPUT);
-   leftServo.attach(LEFTSERVOPIN);	 // Attaches the Left Servo to PWM pin 6 on the Arduino
-   rightServo.attach(RIGHTSERVOPIN);   // Attaches the Right Servo to PWM pin 5 on the Arduino
+   //pinMode(RIGHTSERVOPIN,OUTPUT);
+   //pinMode(LEFTSERVOPIN,OUTPUT);
+   //pinMode(HEADSERVOPIN,OUTPUT);
+   //leftServo.attach(LEFTSERVOPIN);	 // Attaches the Left Servo to PWM pin 6 on the Arduino
+   //rightServo.attach(RIGHTSERVOPIN);   // Attaches the Right Servo to PWM pin 5 on the Arduino
+   //headServo.attach(HEADSERVOPIN);
+   rightServo.attach(RIGHTSERVOPIN);
+   rightServo.setMaximumPulse(2200);
+   leftServo.attach(LEFTSERVOPIN);
+   leftServo.setMaximumPulse(2200);
    headServo.attach(HEADSERVOPIN);
+   headServo.setMaximumPulse(2200);
    goStop();
    Serial.begin(9600);                  // Sets the baud rate to 9600
    turnHeadRight();
    turnHeadLeft();
    turnHeadForward();
+   
+   PgmPrintln("Pi speaker");
+  
+  if (!card.init()) {
+    error("Card init. failed!");
+  }
+  if (!vol.init(card)) {
+    error("No partition!");
+  }
+  if (!root.openRoot(vol)) {
+    error("Couldn't open dir");
+  }
+
+  PgmPrintln("Files found:");
+  root.ls();
 }
 
 void loop() 
@@ -40,6 +72,10 @@ void loop()
   {
     goForward(0);
     delay(700);
+    if(distanceForward > 24)
+    {
+      playfile("AllClear");
+    }
   }
   else
   {
@@ -48,12 +84,14 @@ void loop()
     distanceLeft = lookLeft();
     if(distanceRight > distanceForward && distanceRight > distanceLeft)
     {
+      playfile("TurnRight");
       goRight();
       delay(600);
       goStop();
     }
     else if(distanceLeft > distanceForward && distanceLeft > distanceRight)
     {
+      playfile("TurnLeft");
       goLeft();
       delay(600);
       goStop(); 
@@ -65,7 +103,9 @@ void loop()
       goStop();
     }
   } 
+  SoftwareServo::refresh();
 }
+
 void setupRobot()
 {
     pinMode(RIGHTSERVOPIN,OUTPUT);
@@ -75,6 +115,43 @@ void setupRobot()
    rightServo.attach(RIGHTSERVOPIN);   // Attaches the Right Servo to PWM pin 5 on the Arduino
    headServo.attach(HEADSERVOPIN);
    goStop();
+}
+
+void playfile(char *name) {
+  if (wave.isplaying) {// already playing something, so stop it!
+    wave.stop(); // stop it
+  }
+  if (!file.open(root, name)) {
+    PgmPrint("Couldn't open file ");
+    Serial.print(name); 
+    return;
+  }
+  if (!wave.create(file)) {
+    PgmPrintln("Not a valid WAV");
+    return;
+  }
+  // ok time to play!
+  wave.play();
+}
+/*
+ * print error message and halt
+ */
+void error_P(const char *str) {
+  PgmPrint("Error: ");
+  SerialPrint_P(str);
+  sdErrorCheck();
+  while(1);
+}
+/*
+ * print error message and halt if SD I/O error
+ */
+void sdErrorCheck(void) {
+  if (!card.errorCode()) return;
+  PgmPrint("\r\nSD I/O error: ");
+  Serial.print(card.errorCode(), HEX);
+  PgmPrint(", ");
+  Serial.println(card.errorData(), HEX);
+  while(1);
 }
 
 /*
